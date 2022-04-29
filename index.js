487e9325c0b452b905d297ed231868c32313bfec
Saikat048 <mdsaikathosan048@gmail.com>
2022-04-29 14:55:57 +0600

    load data

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 
-app.get('/', (req, res) => {
-    res.send('Look Mama I can code node now');
-})
+// PASS : PODaGSQLYTVruhf2
 
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://assignment:PODaGSQLYTVruhf2@cluster0.klwxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+
+    try {
+  
+      await client.connect(); 
+      const productCollection = client.db("assignment-product").collection("product");
+
+      app.get('/product', async(req,res) => {
+        const query = {};
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products) 
+      });
+ 
+    } finally { 
+    //   await client.close(); 
+    } 
+  } 
+  run().catch(console.dir);
+ 
 app.listen(port, ()=>{
-    console.log('successfully connected')
+    console.log('successfully connected', port)
 })
