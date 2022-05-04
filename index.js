27b4fbbad33abe31ad006e650f27ff303ee012be
Saikat048 <mdsaikathosan048@gmail.com>
2022-05-05 05:59:57 +0600

    commit delete

--- index.js
+++ index.js
@@ -41,8 +41,14 @@ async function run() {
         res.send(result)
       })
   
+      app.delete('/products/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await productCollection.deleteOne(query);
+        res.send(result)
+      })
 
-     
+ 
     } finally { 
     //   await client.close(); 
     } 
